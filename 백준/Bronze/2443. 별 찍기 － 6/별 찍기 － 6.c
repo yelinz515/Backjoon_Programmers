d1ceca3920037f4021e1f366a668ab6384e9a008
yelinz515 <62506973+yelinz515@users.noreply.github.com>
2022-07-23 14:22:17 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  scanf("%d",&n);
+  
+  for(int i=0;i<n; i++){
+    for(int j=0; j<i; j++){
+      printf(" ");
+      }
+    for(int j=0; j<n-i; j++){
+      printf("*");
+      }
+    for(int k=1; k<n-i; k++){
+      printf("*");
+      }
+  printf("\n");
+  }
+  return 0;
+}
