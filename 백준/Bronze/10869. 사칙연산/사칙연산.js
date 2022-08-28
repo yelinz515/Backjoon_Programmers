14ebe9ba34bef4859e095204eb29bea174d6a6ce
Yerin Kim <ggg06141@gmail.com>
2022-08-28 13:14:22 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.js
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -1,8 +1,8 @@
 let fs = require('fs')
-let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ').map(Number)
 
-let a = parseInt(input[0])
-let b = parseInt(input[1])
+let a = input[0]
+let b = input[1]
 
 console.log(a+b)
 console.log(a-b)
