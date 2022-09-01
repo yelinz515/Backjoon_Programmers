2e1ec6b250eabfb932f675e9e0fe178bde45398b
Yerin Kim <ggg06141@gmail.com>
2022-09-01 23:25:23 +0900

    [Bronze V] Title: 윤년, Time: 132 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.js
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -3,8 +3,6 @@ let input = fs.readFileSync('/dev/stdin').toString()
 
 let year = Number(input)
 
-// 4의 배수 || 100의 배수 아닐 때 = 윤년
-// 4의 배수 || 400의 배수 일 때 = 윤년
 if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
     console.log(1)
 }
