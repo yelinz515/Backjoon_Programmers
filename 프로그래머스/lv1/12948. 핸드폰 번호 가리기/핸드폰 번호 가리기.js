2bff55dea4533e7f58bc1cc6fd226f861fe9fc50
Yerin Kim <ggg06141@gmail.com>
2022-09-06 14:27:22 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,5 @@
+function solution(phone_number) {
+    // 전방 탐색 기호는 ?= 이며, = 다음에 오는 문자가 일치하는 영역에서 제외
+    // 또한, 전방 탐색은 하위 표현식과 같이 소괄호로 감싸주어야만 합니다
+    return phone_number.replace(/\d(?=\d{4})/g, "*")
+}
