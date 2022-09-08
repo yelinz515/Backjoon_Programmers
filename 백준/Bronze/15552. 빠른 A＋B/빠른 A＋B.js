0b05a3dec35a68680acd06f68e0f63b48b39f47a
Yerin Kim <ggg06141@gmail.com>
2022-09-08 18:56:13 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1404 ms, Memory: 171920 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,11 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+let num = +input[0]
+let result = ''
+
+for(let i =1; i<=num; i++){
+    let arr = input[i].split(' ').map(Number)
+    result += arr[0] + arr[1] + '\n'
+}
+console.log(result)
