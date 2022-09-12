ae240bf8201345bfe19f680df4f07c0cd8aee154
Yerin Kim <ggg06141@gmail.com>
2022-09-12 19:12:44 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,9 +1,5 @@
 function solution(n) {
     let num = Math.sqrt(n)
-    if(num > 0 && num % parseInt(num) === 0){
-        return (num+1)**2
-    }
-    else {
-        return -1
-    }
+    if(num > 0 && num % parseInt(num) === 0) return (num+1)**2
+    return -1
 }
