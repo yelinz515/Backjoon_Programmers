d5f33761b2051e49409a1fb4ed8c3b85bf672e9d
Yerin Kim <ggg06141@gmail.com>
2022-09-19 21:46:19 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,8 +1,7 @@
 function solution(n) {
     let sum = ''
     for(let i = 0; i<n; i++){
-        if(i % 2 === 0) sum += "수"
-        else if(i % 2 !== 0) sum += "박"
+        i % 2 === 0 ? sum += "수" : sum += "박"
     }
     return sum 
 }
