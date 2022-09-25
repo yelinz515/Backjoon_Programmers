a4df5de45342c17c0497f30368b8652312461b27
Yerin Kim <ggg06141@gmail.com>
2022-09-25 20:18:58 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -0,0 +1,27 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+let n = input.shift()
+let countGroupWord = 0
+
+for(let i = 0; i<n; i++){
+    let letter = []
+    let word = input[i]
+    let isGroupWord = true; 
+    
+    for (let j = 0; j < word.length; j++) {
+    if (letter.indexOf(word[j]) === -1) {
+      letter.push(word[j]);
+    } else {
+      if (letter.indexOf(word[j]) !== letter.length -1) {
+        isGroupWord = false;
+        break;
+      }
+    }
+    }
+    
+    if (isGroupWord) {
+    countGroupWord += 1;
+    }
+}
+console.log(countGroupWord);
