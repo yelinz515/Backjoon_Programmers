c9cb7064f2efcb31cc6630acc902992e150dadf8
Yerin Kim <ggg06141@gmail.com>
2022-09-26 21:23:58 +0900

    [level 2] Title: 올바른 괄호, Time: 2.85 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,9 @@
+function solution(s){
+    let count = 0;
+    for(let i = 0; i<s.length; i++){
+        s[i] === "(" ? count += 1 : count -= 1
+        if(count < 0) return false
+    }
+    
+    return count === 0 ? true : false
+}
