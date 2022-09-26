b50026606df7a5778213244afdc1f695555b6b18
Yerin Kim <ggg06141@gmail.com>
2022-09-26 19:09:10 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,6 +1,7 @@
 function solution(left, right) {
     let sum = 0
-    for(let i = left; i<=right; i++){ 13 - 17
+    for(let i = left; i<=right; i++){ // 13 - 17
+        // 제곱근이 정수이면 약수의 개수가 홀수
         Number.isInteger(Math.sqrt(i)) ? sum -= i : sum += i                     
     }
     return sum
