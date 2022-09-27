0cd46bf48851b5dfbbc16c8428d768d524f3a403
Yerin Kim <ggg06141@gmail.com>
2022-09-27 21:56:07 +0900

    [level 2] Title: 기능개발, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -0,0 +1,33 @@
+function solution(progresses, speeds) {
+    // 박스 포장 문제와 같음
+    // 100 - progresses[i] = 7, 70, 45
+    // (7, 70, 45) / speeds[i] = 7, Math.ceil(2) = 3, 9
+    // newArr.push([7,3,9])
+    
+    // 첫번 째 수보다 크면 그게 max,
+    // result.push(newArr.splice(0, max)) => [2,1]
+    let rest = progresses.map(i => 100 - i)
+    let newArr = rest.map((i,idx) => Math.ceil(i / speeds[idx]))
+    
+    let max = 0;
+    let result = []
+    while(newArr.length !== 0){
+        // 첫번째 요소보다 큰 수면 만족하는 첫 번째 요소 인덱스 반환, 아니면 -1
+        let n = newArr.findIndex(e => newArr[0] < e)
+        if(n === -1){
+            if(newArr.length > max){
+                max = newArr.length                
+            }
+            result.push(newArr.length)
+            newArr = []
+            
+        } else {
+            if(n > max){
+                max = n
+            }
+            result.push(newArr.splice(0, n).length)
+        }
+    }
+    return result
+    // console.log(max)
+}
