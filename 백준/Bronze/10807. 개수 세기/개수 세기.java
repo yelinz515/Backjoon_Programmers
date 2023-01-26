27af56cd1a870bf0ff859a8c42b742da5b836294
Yerin Kim <ggg06141@gmail.com>
2023-01-26 18:17:11 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -5,22 +5,24 @@ public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[num];
         
-        for(int i=0; i<arr.length; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i < arr.length; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
         
-        int searchNum = Integer.parseInt(br.readLine());
+        int selectNum = Integer.parseInt(br.readLine());
         int count = 0;
         
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == searchNum){
+        for(int i = 0; i < arr.length; i++){
+            if(selectNum == arr[i]){
                 count++;
             }
         }
-        System.out.println(count);
+        System.out.print(count);
+        
     }
+
 }
