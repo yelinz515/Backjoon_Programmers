e782b3a749f4d9b319a9e916a00dc3ab75ff0f74
Yerin Kim <ggg06141@gmail.com>
2023-04-07 11:38:02 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,23 +1,30 @@
 from collections import deque
-def solution(bridge_length, weight, truck_weights): 
-    # 다리 무게
-    bridge_weight = 0
-    # 시간
-    time = 0
-    # 각 트럭의 무게 모음
-    truck_weights = deque(truck_weights)
+def solution(bridge_length, weight, truck_weights):
+    # 트럭 무게
+    truck_w = deque(truck_weights)
     # 현재 다리를 건너고 있는 트럭 무게
-    truck_bridge = deque([0 for _ in range(bridge_length)])
+    bridge_l = deque([0 for _ in range(bridge_length)])
     
-    while truck_bridge:
+    bridge_weight = 0
+    time = 0
+    
+    while bridge_l:
         time += 1
-        if truck_bridge[0] != 0: 
-            bridge_weight -= truck_bridge[0]
-        truck_bridge.popleft()
-        if truck_weights:
-            if bridge_weight + truck_weights[0] <= weight:
-                bridge_weight += truck_weights[0]
-                truck_bridge.append(truck_weights.popleft())
-            else:
-                truck_bridge.append(0)
+        # 만약에 건너고 있는 트럭 무게가 0이 아니면
+        if bridge_l[0] != 0:
+          # 다리 무게에서 현재 다리를 건너고 있는 트럭 무게를 뺀다
+          bridge_weight -= bridge_l[0]
+        # 다리를 지났으면 빠진다
+        bridge_l.popleft()
+        if truck_w:
+          # 다리 무게 + 트럭 무게 가 전체 체중보다 작으면
+          if truck_w[0] + bridge_weight <= weight:
+            # 현재 다리 무게에 트럭 무게를 더해 현재 다리 무게를 업데이트 한다
+            bridge_weight += truck_w[0]
+            # 다리에 트럭을 추가한다.
+            bridge_l.append(truck_w.popleft())
+          # 다리 무게 + 트럭 무게 가 전체 체중보다 크면
+          else:
+            bridge_l.append(0)
+        
     return time
