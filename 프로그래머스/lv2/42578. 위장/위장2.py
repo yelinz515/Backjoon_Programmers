0e4dab87f6278b3088326b23de1a564e156daa2c
Yerin Kim <ggg06141@gmail.com>
2023-04-11 16:08:31 +0900

    Update 위장2.py

--- 프로그래머스/lv2/42578. 위장/위장2.py
+++ 프로그래머스/lv2/42578. 위장/위장2.py
@@ -1,1 +1,10 @@
+from collections import Counter
+from functools import reduce
+def solution(clothes):
+  counter = Counter([type for clothe, type in clothes])
 
+  # reduce(집계 함수, 순회 가능한 데이터[, 초기값])
+  # reduce(lambda acc, cur: acc + cur["age"], users, 0)
+  
+  answer = reduce(lambda acc, cur: acc * (cur + 1), counter.values(), 1)
+  return answer - 1
