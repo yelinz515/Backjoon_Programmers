d620f3aab597176a60f872f5506858b5ea4b8887
Yerin Kim <ggg06141@gmail.com>
2023-04-25 11:01:30 +0900

    Update 추억 점수.java

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -6,7 +6,6 @@ class Solution {
         
         for(int i = 0; i< photo.length; i++) {
             for(int j = 0; j<photo[i].length; j++) {
-                System.out.println();
                 if(Arrays.asList(name).indexOf(photo[i][j]) == -1) continue;
                 else result[i] += yearning[Arrays.asList(name).indexOf(photo[i][j])];
             }
