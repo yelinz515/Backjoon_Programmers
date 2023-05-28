3d450fb3a4e3d582c2b46d016f758cd3e0958222
Yerin Kim <ggg06141@gmail.com>
2023-05-28 19:48:00 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+        
+        return myString.contains(pat) ? 1:0;
+    }
+}
