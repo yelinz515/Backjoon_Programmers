866f6c4232228271f3be1980b52a9c6dc74c18c4
Yerin Kim <ggg06141@gmail.com>
2023-05-31 13:42:45 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.89 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String my_string) {
+        return Arrays.stream(my_string.split("")).distinct().collect(Collectors.joining());
+    }
+}
