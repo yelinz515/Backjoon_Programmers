1800767ffe31c103a82fec8d90aae43746c8defd
Yerin Kim <ggg06141@gmail.com>
2023-05-31 10:47:32 +0900

    [level 0] Title: 이진수 더하기, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String bin1, String bin2) {
+        int answer = Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2);
+        
+        return Integer.toBinaryString(answer);
+    }
+}
